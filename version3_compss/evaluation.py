1449a916adb3681d51915c1721bab99d3ac9d1a0
lucasmsp <lucasmsp@gmail.com>
2018-03-21 22:43:18 -0300

    adding evaluation.py

--- version3_compss/evaluation.py
+++ version3_compss/evaluation.py
@@ -1,10 +1,23 @@
-import pandas as pd
-import numpy as np
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Evaluation - WazeJams.
+
+This program generates some statistics about the
+quality of results: ROC graph, Precision-Recall graph,
+a histogram to analyze the distribution of prevision
+and other statistics.
+"""
+
+__author__ = "Lucas Miguel S Ponce"
+__email__ = "lucasmsp@gmail.com"
+
 import os
+import numpy as np
+import pandas as pd
 np.set_printoptions(threshold=np.nan)
-from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import precision_recall_curve, roc_curve, auc
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
 
 
 def plot_histogram(pred, name):
@@ -14,8 +27,8 @@ def plot_histogram(pred, name):
     plt.title("Predictions Frequency")
     plt.xlabel("Value")
     plt.ylabel("Frequency")
-    plt.xlim((0,1))
-    plt.xticks([0,1])
+    plt.xlim((0, 1))
+    plt.xticks([0, 1])
     name = "{}_bars.png".format(name)
     plt.savefig(name)
 
@@ -29,14 +42,14 @@ def plot_ROC(actual, predictions, name):
     plt.clf()
     plt.title('Receiver Operating Characteristic')
     plt.plot(false_positive_rate, true_positive_rate, 'b',
-    label='AUC = %0.2f'% roc_auc)
+             label='AUC = %0.2f' % roc_auc)
     plt.legend(loc='lower right')
-    plt.plot([0,1],[0,1],'r--')
-    plt.xlim([0,1])
-    plt.ylim([0,1])
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
-    #plt.show()
+    # plt.show()
     plt.savefig(name)
     return roc_auc
 
@@ -57,15 +70,16 @@ def plot_precision_recall(actual, predictions, name):
     name = "{}_PrecisionRecall.png".format(name)
     plt.savefig(name)
 
+
 if __name__ == "__main__":
     import argparse
     p = argparse.ArgumentParser(description='Metrics - WazeJams')
     p.add_argument('-g', '--gridFile', required=True,
-                       help='File with Grids List.')
+                   help='File with Grids List.')
     p.add_argument('-f', '--forecast', required=True,
-                       help='File with the predictions')
+                   help='File with the predictions')
     p.add_argument('-t', '--table', required=True,
-                       help='File with the input data of wazejams (matrix)')
+                   help='File with the input data of wazejams (matrix)')
     arg = vars(p.parse_args())
 
     oficialTable = pd.read_csv(arg['table'], sep=',', header=None)
@@ -76,7 +90,7 @@ if __name__ == "__main__":
     forecasts.columns = ['IDgrid', 'last_timestamp', 'average',
                          'variance', 'ci_95', 'ci_95.1', 'percentage']
     forecasts["percentage"] = forecasts["percentage"].apply(np.round)
-    forecasts['last_timestamp'] = '2017-02-08 00:00:00' #!
+
     forecasts = forecasts.merge(Grids, how='left', on='IDgrid')
     forecasts["True"] = np.nan
     instance = forecasts['last_timestamp'].iloc[0]
@@ -89,16 +103,15 @@ if __name__ == "__main__":
     N_green = len(forecasts[forecasts.percentage == 0])
     N_total = len(forecasts)
     N_red = N_total - N_green
-    acc = 100.0*len(forecasts[forecasts.Right == True]) / N_total
+    acc = 100.0 * len(forecasts[forecasts.Right == True]) / N_total
 
     true = forecasts['True'].values
     pred = forecasts['percentage'].values
-    base = os.path.basename(arg['forecast']).replace(".txt","")
-    auc = plot_ROC(true, pred, base) * 100
+    base = os.path.basename(arg['forecast']).replace(".txt", "")
+    AUC = plot_ROC(true, pred, base) * 100
     plot_precision_recall(true, pred, base)
     # plot_histogram(true, base)
     # plot_histogram(pred,' base)
-
 
     print "*" * 20
     print """
@@ -107,5 +120,5 @@ if __name__ == "__main__":
     Number of predicted grids w/o traffic jams: {}
     Accuracy: {:.2f}%
     AUC: {:.2f}%
-    """.format(N_total, N_red, N_green, acc, auc)
+    """.format(N_total, N_red, N_green, acc, AUC)
     print "*" * 20
